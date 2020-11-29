de05794afb3da721fa24727b6d1c8a58bd1e2e4e
yaboiishere <mixaildobrev@gmail.com>
2020-11-29 19:23:38 +0200

    fix fix crash when user id non existent

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,5 @@
 class ApplicationJob < ActiveJob::Base
-    Telegram::Bot::Client::AsyncJob.rescue_from(Telegram::Bot::Error) do |error|
+    Telegram::Bot::Client.default_async_job.rescue_from(Telegram::Bot::Error) do |error|
         Rails.logger.info error.inspect
     end
 end
